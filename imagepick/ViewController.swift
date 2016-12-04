92e03337ba3b78394b9a73c09ddf17814d09fab4
viniethepoo <vinayrai007@aol.com>
2016-12-04 17:35:37 +0530

    keyboard notification

--- imagepick/ViewController.swift
+++ imagepick/ViewController.swift
@@ -21,8 +21,42 @@ UINavigationControllerDelegate {
         // Do any additional setup after loading the view, typically from a nib.
     
     }
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        self.view.endEditing(true)
+    //override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    //    self.view.endEditing(true)
+    //}
+    override func viewWillAppear(_ animated: Bool) {
+        
+        super.viewWillAppear(animated)
+        subscribeToKeyboardNotifications()
+    }
+    
+   
+    func subscribeToKeyboardNotifications() {
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: .UIKeyboardWillShow, object: nil)
+    }
+    
+    func unsubscribeFromKeyboardNotifications() {
+        
+        NotificationCenter.default.removeObserver(self, name: .UIKeyboardWillShow, object: nil)
+    }
+    //When the keyboardWillShow notification is received, shift the view's frame up
+    func keyboardWillShow(_ notification:Notification) {
+        
+        view.frame.origin.y -= getKeyboardHeight(notification)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        
+        super.viewWillDisappear(animated)
+        unsubscribeFromKeyboardNotifications()
+    }
+    
+    func getKeyboardHeight(_ notification:Notification) -> CGFloat {
+        
+        let userInfo = notification.userInfo
+        let keyboardSize = userInfo![UIKeyboardFrameEndUserInfoKey] as! NSValue // of CGRect
+        return keyboardSize.cgRectValue.height
     }
     @IBAction func ImagePick(_ sender: AnyObject) {
         let pickercontroller = UIImagePickerController()
